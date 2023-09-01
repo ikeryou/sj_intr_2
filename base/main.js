6f4c29b017f6baf0483f804aa065aefdb1f57faf
ikeryou <ikeda.ryou@gmail.com>
2023-09-01 14:50:30 +0900

    update

--- base/main.js
+++ base/main.js
@@ -1,40 +1,44 @@
+// ボタンクラス宣言
 class MyButton {
 
   constructor(el) {
+    // 操作する要素
     this.el = el
 
+    // アニメーションさせる背景
     const bg = document.createElement('span')
     bg.classList.add('js-button-bg')
     this.el.prepend(bg)
     this.bg = bg
 
+    // 背景、最初は見えないように
     gsap.set(this.bg, {
       scaleX: 0,
     })
-
-    this.el.addEventListener('mouseenter', this._eRollOver.bind(this))
-    this.el.addEventListener('mouseleave', this._eRollOut.bind(this))
   }
 
+  // イベント ロールオーバー
   _eRollOver() {
     this._startRollOverMotion()
   }
 
+  // ロールオーバーモーション開始
   _startRollOverMotion() {
     this.el.classList.add('-hover')
     gsap.to(this.bg, {
       scaleX: 1,
-      duration: 0.25,
+      duration: 0.75,
       delay: 0,
       ease: Expo.easeOut,
     })
   }
 
-
+  // イベント ロールアウト
   _eRollOut() {
     this._startRollOutMotion()
   }
 
+  // ロールアウトモーション開始
   _startRollOutMotion() {
     this.el.classList.remove('-hover')
     gsap.to(this.bg, {
@@ -52,7 +56,6 @@ class MyButton {
 
 
 init()
-
 function init() {
   document.querySelectorAll('.js-button').forEach((el) => {
     new MyButton(el)
--- finish/main.js
+++ finish/main.js
@@ -1,73 +1,119 @@
+// ボタンクラス宣言
 class MyButton {
 
   constructor(el) {
+    // アニメーション中かどうか
     this.isPlaying = false
+
+    // ロールオーバーしてるかどうか
     this.isHover = false
 
+    // 操作する要素
     this.el = el
 
+    // アニメーションさせる背景
     const bg = document.createElement('span')
     bg.classList.add('js-button-bg')
     this.el.prepend(bg)
     this.bg = bg
 
+    // 背景、最初は見えないように
     gsap.set(this.bg, {
       scaleX: 0,
     })
 
-    this.el.addEventListener('mouseenter', this._eRollOver.bind(this))
-    this.el.addEventListener('mouseleave', this._eRollOut.bind(this))
+    // イベント登録
+    this.el.addEventListener('mouseenter', () => {
+      this._eRollOver()
+    })
+    this.el.addEventListener('mouseleave', () => {
+      this._eRollOut()
+    })
   }
 
+  // イベント ロールオーバー
   _eRollOver() {
+    // ロールオーバーフラグをtrueに
     this.isHover = true
+
+    // アニメーション中ならなにもしない
     if(!this.isPlaying) {
+      // モーション開始
       this._startRollOverMotion()
     }
   }
 
+  // ロールオーバーモーション開始
   _startRollOverMotion() {
+    // アニメーションフラグをtrueに
     this.isPlaying = true
+
+    // ロールオーバーモーション中のclassを付与
+    // 文字の色はcssで
     this.el.classList.add('-hover')
+
+    // 背景要素モーション開始
     gsap.to(this.bg, {
       scaleX: 1,
       duration: 0.25,
       delay: 0,
       ease: Expo.easeOut,
-      onComplete: this._eCompleteRollOver.bind(this)
+      onComplete: () => {
+        this._eCompleteRollOver()
+      }
     })
   }
 
+  // イベント ロールオーバーモーション完了
   _eCompleteRollOver() {
+    // アニメーションフラグをfalseに
     this.isPlaying = false
 
+    // この時点でロールオーバーが外れてたらロールアウトのモーションを開始する
     if(!this.isHover) {
       this._eRollOut()
     }
   }
 
+  // イベント ロールアウト
   _eRollOut() {
+    // ロールオーバーフラグをfalseに
     this.isHover = false
+
+    // アニメーション中ならなにもしない
     if(!this.isPlaying) {
+      // ロールアウトモーション開始
       this._startRollOutMotion()
     }
   }
 
+  // ロールアウトモーション開始
   _startRollOutMotion() {
+    // アニメーションフラグをtrueに
     this.isPlaying = true
+
+    // class付与
+    // 文字の色はcssで
     this.el.classList.remove('-hover')
+
+    // モーション開始
     gsap.to(this.bg, {
       scaleX: 0,
       duration: 0.75,
       delay: 0,
       ease: Expo.easeInOut,
-      onComplete: this._eCompleteRollOut.bind(this)
+      onComplete: () => {
+        this._eCompleteRollOut()
+      }
     })
   }
 
+  // イベント ロールアウトモーション完了
   _eCompleteRollOut() {
+    // アニメーションフラグfalseに
     this.isPlaying = false
 
+    // この時点でロールオーバーしてたらロールオーバーモーション開始
     if(this.isHover) {
       this._eRollOver()
     }
@@ -76,11 +122,7 @@ class MyButton {
 
 
 
-
-
-
 init()
-
 function init() {
   document.querySelectorAll('.js-button').forEach((el) => {
     new MyButton(el)
